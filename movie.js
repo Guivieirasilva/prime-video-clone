d916a39cddd86bbdf96b6060182af72376a05cd9
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-04-02 10:10:19 -0300

    nova API
    consumindo uma nova API

--- movie.js
+++ movie.js
@@ -1,23 +1,13 @@
+//API
+const API_KEY = 'api_key=5a21c7e33fd419b270df295a1b95435d';
+const BASE_URL  = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+
+
 var main = document.querySelectorAll('.main .center')[0];
 
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb025c13349acc05dabebf9604&language=en-US&page=1')
-.then(response => response.json())
-.then(data => {
-        data.results.map((i,j) => {
-
-            if(j > 10)
-                return;
-            if(j == 0){
-                main.innerHTML+=`
-                
-                <div class="banner-principal" style="background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
-
-                
-                `;
-
-
-            }
-
-
-        })
+fetch(API_URL)
+.then(response=> response.json())
+.then(data=> {
+    console.log(data)
 })

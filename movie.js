2bb864fce889e56cb8538928675445e42582f4f8
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-04-02 10:58:16 -0300

    requisição para page 2

--- movie.js
+++ movie.js
@@ -1,5 +1,5 @@
 //API
-const API_KEY = 'api_key=5a21c7e33fd419b270df295a1b95435d';
+const API_KEY = 'api_key=5a21c7e33fd419b270df295a1b95435d&page=1';
 const BASE_URL  = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 
@@ -10,7 +10,7 @@ fetch(API_URL)
 .then(data=> {
     data.results.map((i,j)=>{
 
-        if(j > 10)
+        if(j > 17)
             return
         if(j == 0){
             main.innerHTML+=`
@@ -25,7 +25,36 @@ fetch(API_URL)
                 <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
                 `
             }else{
+                main.innerHTML+=`
+                <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
+                `
+            }
+        }
+    })
+})
+fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5a21c7e33fd419b270df295a1b95435d&page=2')
+.then(response=> response.json())
+.then(data=> {
+    data.results.map((i,j)=>{
 
+        if(j > 18)
+            return
+        if(j == 0){
+            main.innerHTML+=`
+            
+                <div class="banner-principal" style="background-image:url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"</div>
+            
+            `
+        }else{
+            if(j == 1){
+                main.innerHTML+=`
+                <h2>Assista o melhor filme</h2>
+                <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
+                `
+            }else{
+                main.innerHTML+=`
+                <div class="card-movie"><img src="https://image.tmdb.org/t/p/w500`+i.poster_path+`" /></div>
+                `
             }
         }
     })
